3708dd4982ec39106654df6a82e3272721cdb8ca
Andrey Aleksandrov <postgred@gmail.com>
2018-03-12 21:48:34 +0300

    Fix encoded span for http transport

--- post-py/post_app.py
+++ post-py/post_app.py
@@ -50,7 +50,7 @@ def http_transport(encoded_span):
     # The collector expects a thrift-encoded list of spans. Instead of
     # decoding and re-encoding the already thrift-encoded message, we can just
     # add header bytes that specify that what follows is a list of length 1.
-    body = '\x0c\x00\x00\x00\x01' + encoded_span
+    body = '\x0c\x00\x00\x00\x01' + str(encoded_span)
     requests.post(ZIPKIN_URL, data=body,
                   headers={'Content-Type': 'application/x-thrift'})
 
