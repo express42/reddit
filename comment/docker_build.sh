b668e566b62d51e0c04960daaa8d929bec34ec87
Alexander Suleymanov <asu@express42.com>
2018-12-21 13:41:06 +0300

    Removed "logging" tag from docker_build.sh

--- comment/docker_build.sh
+++ comment/docker_build.sh
@@ -3,4 +3,4 @@
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
 
-docker build -t $USER_NAME/comment:logging .
+docker build -t $USER_NAME/comment .
